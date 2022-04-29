281cc777507c57ff58f82b69ecaaf45c443a781d
zearakun <91083604+zearakun@users.noreply.github.com>
2022-04-29 21:38:10 +0900

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ print("""
 ██║░░██║██║░░░██║░░░██║░░██║██║░░░░░██║░░██║  ██╔══██╗██╔══██║██║██║░░██║██╔══╝░░██╔══██╗
 ██████╔╝██║░░░██║░░░╚█████╔╝███████╗╚█████╔╝  ██║░░██║██║░░██║██║██████╔╝███████╗██║░░██║
 ╚═════╝░╚═╝░░░╚═╝░░░░╚════╝░╚══════╝░╚════╝░  ╚═╝░░╚═╝╚═╝░░╚═╝╚═╝╚═════╝░╚══════╝╚═╝░░╚═╝
-
 [1]入る
 [2]抜ける
 [3]送信
@@ -20,12 +19,15 @@ choice = input("1/2/3:")
 
 #入れないから最初にはいっておいて
 if choice == "1":
+   link = input("link:")
    with open('tokens.txt', 'r') as f:
       token = f.readlines()
    for i in token:
-     link = input("link:")
-     apilink = "https://discordapp.com/api/v9/invites/" + link
-     headers={'authorization': i}
+     apilink = "https://discord.com/api/v9/invites/" + link
+     headers={
+       'authorization': i,
+       'content-type': 'application/json'
+     }
      bot_invite = requests.post(apilink, headers=headers)
 
 if choice == "2":
@@ -33,7 +35,7 @@ if choice == "2":
   with open('tokens.txt', 'r') as f:
       token = f.readlines()
   for i in token:
-    headers={'Authorization': i}
+    headers={'authorization': i}
     requests.delete(f"https://discord.com/api/v9/users/@me/guilds/{guild}",headers=headers)
 
 if choice == "3":
@@ -46,8 +48,8 @@ if choice == "3":
   for i in token:
     for a in channel:
       for u in content:
-        for nu in range(30):
+        for nu in range(20):
             url = 'https://discord.com/api/v9/channels/'+a+'/messages'
-            data = {"content": f"{u} {random.choice(range(1000,99999))}"}
+            data = {"content": f"{nu}.{u} #ditoloはこの活動を支援しています {random.choice(range(1000,99999))}"}
             header = {"authorization": i}
             r = requests.post(url, data=data, headers=header)
