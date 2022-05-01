4a79fa5b9ef5e1d19b64e1e9288c869d75849e3a
zearakun <91083604+zearakun@users.noreply.github.com>
2022-05-01 14:40:44 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import requests
 import random
-from time import time
 
 
 print("""
@@ -17,42 +16,30 @@ print("""
 
 choice = input("1/2/3:")
 
-#入れないから最初にはいっておいて
-if choice == "1":
-   link = input("link:")
-   with open('tokens.txt', 'r') as f:
-      token = f.readlines()
-   for i in token:
-     apilink = "https://discord.com/api/v9/invites/" + link
-     headers={
-       'authorization': i,
-       'content-type': 'application/json'
-     }
-     bot_invite = requests.post(apilink, headers=headers)
+
 
 if choice == "2":
   guild = input("guildid:")
   with open('tokens.txt', 'r') as f:
-      token = f.readlines()
+      token = f.read().splitlines()
   for i in token:
     headers={'authorization': i}
     requests.delete(f"https://discord.com/api/v9/users/@me/guilds/{guild}",headers=headers)
 
 if choice == "3":
-  with open('token.txt', 'r') as f:
-      token = f.readlines()
+  with open('tokens.txt', 'r') as f:
+      token = f.read().splitlines()
   with open('channel.txt', 'r') as f:
-      channel = f.readlines()
+      channel = f.read().splitlines()
   with open('content.txt', 'r') as f:
-      content = f.readlines()
-  for i in token:
-    for a in channel:
-      for u in content:
-        for nu in range(3):
+      content = f.read()
+  for nu in range(100):
+            a = random.choice(channel)
             url = 'https://discord.com/api/v9/channels/'+a+'/messages'
             en = ["a","b","c","d","i","e","f","g","h","i","j","k","n","m","l","o","p","q","r","s","","t","u","v","w","s","y","g"]
             enran = f"{random.choice(en)}{random.choice(en)}{random.choice(en)}"
-            data = {"content": f"{u} #ditoloはこの活動を支援しています {random.choice(range(0,99999))}{enran}"}
+            data = {"content": f"{content} #ditoloはこの活動を支援しています {random.choice(range(0,99999))}{enran}"}
+            i = random.choice(token)
             header = {"authorization": i}
             r = requests.post(url, data=data, headers=header)
             if r.status_code == 200:
